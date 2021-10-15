3d6206eca35091d3b0238821e454cf2b491ffbd9
Mario Autherith <m.autherith@eyedea.at>
2021-10-15 16:50:38 +0200

    add login functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,61 @@
 <template>
   <div class="container my-5">
     <h1>A lot of Tests in here</h1>
-    <Events />
+    <Events 
+      v-if="user"
+    />
+    <div
+      v-else
+      class="login"
+    >
+      <form>
+        <div class="mb-3">
+          <label class="form-label">Email address</label>
+          <input type="email" v-model="email" class="form-control">
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Password</label>
+          <input type="password" v-model="pw" class="form-control">
+        </div>
+        <div
+          class="btn btn-primary"
+          @click="login"
+        >
+          Submit
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+export default {
+  data() {
+    return {
+      user: undefined,
+      email: undefined,
+      pw: undefined
+    }
+  },
+  mounted() {
+      const auth = getAuth();
+      if(auth.currentUser){
+        this.user = user
+      }
+  },
+  methods: {
+    login() {
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, this.email, this.pw)
+        .then((userCredential) => {
+          this.user = userCredential.user;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }
+  }
+}
 </script>
