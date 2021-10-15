c931bdfecca0ba158e27654f94f09602275b241d
Mario Autherith <m.autherith@eyedea.at>
2021-10-15 15:36:21 +0200

    setup and simple data fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,45 @@
 <template>
-  <Tutorial/>
+  <div class="container my-5">
+    <h1>A lot of Tests in here</h1>
+    <div 
+      v-if="events"
+      class="event-cards my-5"
+    >
+      <div
+        class="card col-12 col-lg-6"
+        v-for="event in events"
+        v-bind:key="event.id" 
+      >
+        <h2>{{ event.data.title }}</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { db } from "~/plugins/firebase.js"
+import { collection, getDocs } from 'firebase/firestore'
+export default {
+  data() {
+    return {
+      events: []
+    }
+  },
+  async fetch() {
+    try {
+      const events = [];
+      const querySnapshot = await getDocs(collection(db, 'events'));
+      querySnapshot.forEach((doc) => {
+        const event = {
+          id: doc.id,
+          data: doc.data()
+        }
+        events.push(event);
+      })
+      this.events = events;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
 </script>
